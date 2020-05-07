5572f9f69fd9e1ccee7279bf4505a9a96705c9c9
Taha Shabani <taha.shabani.m@gmail.com>
2020-05-07 18:54:28 +0430

    first implementation of filter done, but specific urls not handled

--- src/main/java/com/loghme/service/AuthenticationFilter.java
+++ src/main/java/com/loghme/service/AuthenticationFilter.java
@@ -1,4 +1,44 @@
 package com.loghme.service;
 
-public class AuthenticationFilter {
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        System.out.println(
+                "Logging Request  {} : {}" + req.getMethod() +
+                        req.getRequestURI());
+        chain.doFilter(request, response);
+        System.out.println((
+                "Logging Response :{}"+
+                        res.getContentType()));
+    }
+
+    @Bean
+    public FilterRegistrationBean<AuthenticationFilter> loggingFilter(){
+        FilterRegistrationBean<AuthenticationFilter> registrationBean
+                = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new AuthenticationFilter());
+        registrationBean.addUrlPatterns("*");
+        System.out.println("heeeeeeee");
+
+        return registrationBean;
+    }
 }
