0dac8ea9a15e8edcd55acdc9ca1f9092e0e7b1d4
Taha Shabani <taha.shabani.m@gmail.com>
2020-05-08 05:53:06 +0430

    filter claims added to servlet request, 401 and 403 errors remaining

--- src/main/java/com/loghme/service/AuthenticationFilter.java
+++ src/main/java/com/loghme/service/AuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.loghme.service;
 
 import com.loghme.domain.utils.Loghme;
+import io.jsonwebtoken.Claims;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -38,16 +39,23 @@ public class AuthenticationFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
-        String authTokenHeader = req.getHeader("Authorization");
-        String[] parts = authTokenHeader.split(" ");
-        String token = parts[1];
-
         try {
-            Loghme.getInstance().decodeJWT(token);
-            chain.doFilter(request, response);
-            System.out.println("token is valid!");
+            String authTokenHeader = req.getHeader("Authorization");
+            System.out.println(authTokenHeader);
+            if (authTokenHeader.length() > 7) { //Bearer
+                String[] parts = authTokenHeader.split(" ");
+                String token = parts[1];
+                Claims claims = Loghme.getInstance().decodeJWT(token);
+                req.setAttribute("claims", claims);
+                chain.doFilter(request, response);
+            }
+            else {
+                //null token -> 401
+                System.out.println("empty token");
+            }
         }
         catch (Exception e) {
+            //invalid token -> 403
             System.out.println("token is invalid!");
         }
     }
